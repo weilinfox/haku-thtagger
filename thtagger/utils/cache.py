f27e8ba82d82e341dec7faf6686994bc440cb884
weilinfox <weilin.king@qq.com>
2022-08-21 22:44:22 +0800

    ⚙ 若 XDG_CACHE_HOME 存在则使用之

--- thtagger/utils/cache.py
+++ thtagger/utils/cache.py
@@ -9,4 +9,4 @@ def get_cache_path():
     if sys.platform in ["win32", "cygwin"]:
         return os.path.join(os.getenv("LOCALAPPDATA"), cache_dirname)
     else:
-        return os.path.join(os.path.join(os.getenv("HOME"), ".cache"), cache_dirname)
+        return os.path.join(os.environ.get('XDG_CACHE_HOME', os.path.expanduser("~/.cache")), cache_dirname)
